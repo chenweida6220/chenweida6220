bd27ca1a89f887cdbf88e6333799c8e266976174
Wei Chen <weida.wdc@gmail.com>
2025-10-19 22:55:55 -0400

    adjusting namecard size for mobile (2)

--- Portfolio Items/tempCodeRunnerFile.py
+++ Portfolio Items/tempCodeRunnerFile.py
@@ -1,2 +1,6 @@
-viewBox='0 0 800 400',
+dwg = svgwrite.Drawing(
+    'dark-mode_namecard.svg',
+    size=('100%', '100%'),
+    # viewBox='0 0 800 400',
     # preserveAspectRatio='xMidYMid meet'
+)
